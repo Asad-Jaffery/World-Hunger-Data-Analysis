aac5f65fdf0f49a6ed90bbf93b4364f6242bc896
belizo <beliz@uw.edu>
2022-11-14 18:23:19 -0800

    knit test 2

--- source/summary_information_version2.R
+++ source/summary_information_version2.R
@@ -1,19 +1,9 @@
 library("tidyverse")
 
 country_wide_average <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/country-wide-average.csv")
-undernourishment <- read.csv("~/info201/project-AsadJafferyy/data/prevalence-of-undernourishment.csv")
-hunger_index <- read.csv("~/info201/project-AsadJafferyy/data/global-hunger-index.csv")
 
 
 View(country_wide_average)
-View(global_malnutrition)
-View(undernourishment)
-View(hunger_index)
-
-summary_info <- list()
-
-
-# global_malnutrition information 
 
 
 # Country with highest percentage of underweight population 
@@ -22,47 +12,8 @@ highest_underweight <- country_wide_average %>%
   pull(Country)
 
 highest_underweight <- function(highest_underweight){
-  t <- paste(highest_underweight)
+  t <- paste0("Country with the highest percentage of underweight population is ", highest_underweight)
   return(t) 
 }
 
 
-# Country with lowest percentage of underweight population
-summary_info$lowest_underweight <- country_wide_average %>% 
-  filter(Underweight == min(Underweight, na.rm = TRUE)) %>%
-  pull(Country)
-
-
-# Country with highest percentage of undernourished population (recent date)
-
-summary_info$highest_undernourishmnt <- undernourishment %>%
-  filter(Year == max(Year, na.rm = TRUE)) %>%
-  filter(Prevalence.of.undernourishment....of.population. == max(Prevalence.of.undernourishment....of.population.,na.rm = TRUE))%>%
-  pull(Entity)
-
-
-
-# Country with lowest percentage of undernourished population (recent date)
-
-summary_info$highest_undernourishmnt <- undernourishment %>%
-  filter(Year == max(Year, na.rm = TRUE)) %>%
-  filter(Prevalence.of.undernourishment....of.population. == min(Prevalence.of.undernourishment....of.population.,na.rm = TRUE))%>%
-  pull(Entity)
-
-
-# Country with the highest GHI (Global Hunger Index) score in the most recent year the data records
-
-summary_info$highest_GHI <- hunger_index %>%
-  filter(Year == max(Year, na.rm = TRUE)) %>%
-  filter(Global.Hunger.Index..2021. == max(Global.Hunger.Index..2021., na.rm = TRUE)) %>%
-  pull(Entity)
-
-
-# Country with the lowest GHI (Global Hunger Index) score in the most recent year the data records
-
-summary_info$lowest_GHI <- hunger_index %>%
-  filter(Year == max(Year, na.rm = TRUE)) %>%
-  filter(Global.Hunger.Index..2021. == min(Global.Hunger.Index..2021., na.rm = TRUE)) %>%
-  pull(Entity)
-
-View(summary_info)
