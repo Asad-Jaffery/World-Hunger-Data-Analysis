a979adaed1e1643f1547e9ba4435e74b9451be2a
belizo <beliz@uw.edu>
2022-11-14 17:10:14 -0800

    created aggregated table

--- source/aggregated_table_undernourishment_data.R
+++ source/aggregated_table_undernourishment_data.R
@@ -5,11 +5,12 @@ undernourishment_data <- read.csv("https://raw.githubusercontent.com/info201b-au
 colnames(undernourishment_data)[4] = "Prevalence_of_Undernourishment"
 colnames(undernourishment_data)[1] = "Country"
 
-# Group_by() dataset
+# Group_by() table
 undernourishment_table <- undernourishment_data %>% 
   select(Country, Year, Prevalence_of_Undernourishment) %>%
   group_by(Country)
 
 # View table
 View(undernourishment_table)
+
 
