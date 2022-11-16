253471ef46c918f3a05f7499ab5343aa150b8b5e
ctanakaa <ctanakaa@uw.edu>
2022-11-15 16:41:19 -0800

    updated functions

--- source/summary_information.R
+++ source/summary_information.R
@@ -1,60 +1,77 @@
 library("tidyverse")
 
-country_wide_average <- read.csv("~/info201/project-AsadJafferyy/data/country-wide-average.csv")
-undernourishment <- read.csv("~/info201/project-AsadJafferyy/data/prevalence-of-undernourishment.csv")
-hunger_index <- read.csv("~/info201/project-AsadJafferyy/data/global-hunger-index.csv")
-
+country_wide_average <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/country-wide-average.csv")
+global_hunger_index <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/global-hunger-index.csv")
+undernourishment_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/prevalence-of-undernourishment.csv")
 
 View(country_wide_average)
-View(undernourishment)
-View(hunger_index)
-
-summary_info <- list()
-
-
-# global_malnutrition information 
 
 
 # Country with highest percentage of underweight population 
-summary_info$highest_underweight <- country_wide_average %>% 
+highest_underweight1 <- country_wide_average %>% 
   filter(Underweight == max(Underweight, na.rm = TRUE)) %>%
   pull(Country)
 
+highest_underweight <- function(){
+  t <- paste("Country with the highest percentage of underweight population is ", highest_underweight1)
+  return(t) 
+}
+
+
 # Country with lowest percentage of underweight population
-summary_info$lowest_underweight <- country_wide_average %>% 
+lowest_underweightr <- country_wide_average %>% 
   filter(Underweight == min(Underweight, na.rm = TRUE)) %>%
   pull(Country)
 
+lowest_underweight <- function(){
+  t <- paste("Country with the lowest percentage of underweight population is ", lowest_underweightr)
+  return(t)
+}
 
 # Country with highest percentage of undernourished population (recent date)
 
-summary_info$highest_undernourishmnt <- undernourishment %>%
+highest_undernourishment1 <- undernourishment_data %>%
   filter(Year == max(Year, na.rm = TRUE)) %>%
   filter(Prevalence.of.undernourishment....of.population. == max(Prevalence.of.undernourishment....of.population.,na.rm = TRUE))%>%
   pull(Entity)
 
+highest_undernourishment <- function(){
+  t <- paste("Country with the highest percentage of undernourished population is ", highest_undernourishment1)
+  return(t)
+}
 
 # Country with lowest percentage of undernourished population (recent date)
 
-summary_info$highest_undernourishmnt <- undernourishment %>%
+lowest_undernourishment1 <- undernourishment_data %>%
   filter(Year == max(Year, na.rm = TRUE)) %>%
   filter(Prevalence.of.undernourishment....of.population. == min(Prevalence.of.undernourishment....of.population.,na.rm = TRUE))%>%
   pull(Entity)
 
+lowest_undernourishment <- function(){
+  t <- paste("Country with the lowest percentage of undernourished population is ", highest_undernourishment1)
+  return(t)
+}
 
 # Country with the highest GHI (Global Hunger Index) score in the most recent year the data records
 
-summary_info$highest_GHI <- hunger_index %>%
+highest_GHI1 <- global_hunger_index %>%
   filter(Year == max(Year, na.rm = TRUE)) %>%
   filter(Global.Hunger.Index..2021. == max(Global.Hunger.Index..2021., na.rm = TRUE)) %>%
   pull(Entity)
 
+highest_GHI <- function(){
+  t <- paste("Country with the highest GHI (Global Hunger Index) score is ", highest_GHI1)
+  return(t)
+}
 
 # Country with the lowest GHI (Global Hunger Index) score in the most recent year the data records
 
-summary_info$lowest_GHI <- hunger_index %>%
+lowest_GHI1 <- global_hunger_index %>%
   filter(Year == max(Year, na.rm = TRUE)) %>%
   filter(Global.Hunger.Index..2021. == min(Global.Hunger.Index..2021., na.rm = TRUE)) %>%
   pull(Entity)
 
-View(summary_info)
+lowest_GHI <- function(){
+  t <- paste("Country with the lowest GHI (Global Hunger Index) score is ", lowest_GHI1)
+  return(t)
+}
