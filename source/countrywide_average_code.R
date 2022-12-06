253320f1e565b99962c839a3eb97adee45f74a49
asadjafferyy <asadjaf@uw.edu>
2022-12-05 17:16:14 -0800

    asads random push

--- source/countrywide_average_code.R
+++ source/countrywide_average_code.R
@@ -5,7 +5,7 @@ library("ggplot2")
 
 # Load dataset
 country_wide_average <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/country-wide-average.csv")
-View(country_wide_average)
+
 
 # Function 1
 get_country_underweight_pop <- function() {
