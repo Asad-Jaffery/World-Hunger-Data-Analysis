218300582501e39429bc5d8a752dfe2f816beff4
belizo <beliz@uw.edu>
2022-12-05 17:04:16 -0800

    made first interactive chart!

--- source/app_server.R
+++ source/app_server.R
@@ -4,3 +4,28 @@ library(ggplot2)
 library(tidyr)
 
 #------------------------------------------------------------------------------#
+# interactive visualization one 
+
+undernourishment_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AsadJafferyy/main/data/prevalence-of-undernourishment.csv")
+colnames(undernourishment_data)[4] = "Prevalence_of_Undernourishment"
+colnames(undernourishment_data)[1] = "Country"
+
+data_undernourishment <- undernourishment_data %>% 
+  na.omit() %>%
+  group_by(Country) %>%
+  select(Country, Year, Prevalence_of_Undernourishment)
+
+server <- function(input, output){
+    
+  output$distPlot <- renderPlot({
+    
+    ggplot(data_undernourishment %>% filter(Country == input$Country)) +
+      geom_line(mapping = aes(x = Year, y = Prevalence_of_Undernourishment,
+                              color = Country)) + 
+      labs(title = "Prevalence of Undernourishment by Country from 2001 to 2019", 
+           x = "Year",
+           y = "Prevalence of Undernourishment")
+  })
+}
+
+#------------------------------------------------------------------------------#
--- source/app_ui.R
+++ source/app_ui.R
@@ -1,4 +1,29 @@
 library(shiny)
 source("app_server.R")
 
+
 #------------------------------------------------------------------------------#
+# page two content
+page_two <- tabPanel(
+  "Interactive Visualization",
+  titlePanel("Prevalence of Undernourishment by Country from 2001 to 2019"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "Country",
+                  label = "Select Country",
+                  choices = c(data_undernourishment$Country))
+    ),
+  mainPanel(
+    plotOutput("distPlot")
+  )
+ )
+)
+
+#------------------------------------------------------------------------------#
+# combine pages
+ui <- navbarPage(
+  "Combatting World Hunger Through Data Analysis",
+  page_two
+)
+
